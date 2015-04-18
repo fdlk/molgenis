c6c71e21fa1c5ac8fd6e104e2e3c2c1ddf25f7b7
Fleur Kelpin <f.kelpin@umcg.nl>
2015-04-18 15:45:28 +0200

    Disable the nonexisting tests.

--- molgenis-ontocat/src/test/java/org/molgenis/ontocat/bioportal/BioPortalOntologyServiceTest.java
+++ molgenis-ontocat/src/test/java/org/molgenis/ontocat/bioportal/BioPortalOntologyServiceTest.java
@@ -2,80 +2,96 @@ package org.molgenis.ontocat.bioportal;
 
 import org.testng.annotations.Test;
 
-public class BioPortalOntologyServiceTest {
+public class BioPortalOntologyServiceTest
+{
 
-  @Test
-  public void BioPortalOntologyService() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void BioPortalOntologyService()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void BioPortalOntologyServiceString() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void BioPortalOntologyServiceString()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void conceptIriToId() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void conceptIriToId()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void convertJsonStringToOntology() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void convertJsonStringToOntology()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void convertJsonStringToOntologyTerm() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void convertJsonStringToOntologyTerm()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void convertJsonStringToOntologyTerms() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void convertJsonStringToOntologyTerms()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void convertJsonToOntologies() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void convertJsonToOntologies()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getChildren() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getChildren()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getHttpResponse() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getHttpResponse()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getOntologies() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getOntologies()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getOntology() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getOntology()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getProxyCountForOntology() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getProxyCountForOntology()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void getRootTerms() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void getRootTerms()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void processUrl() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void processUrl()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 
-  @Test
-  public void recursivelyPageChildren() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test(enabled = false)
+	public void recursivelyPageChildren()
+	{
+		throw new RuntimeException("Test not implemented");
+	}
 }

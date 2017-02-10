0f61c8b12b73dc67602f107e00a358f29d72526d
Fleur Kelpin <f.kelpin@umcg.nl>
2017-02-10 22:01:44 +0100

    Update graphiql to newer version

--- molgenis-graphql/src/main/java/org/molgenis/graphql/plugin/GraphQLPluginController.java
+++ molgenis-graphql/src/main/java/org/molgenis/graphql/plugin/GraphQLPluginController.java
@@ -2,11 +2,9 @@ package org.molgenis.graphql.plugin;
 
 import com.bretpatterson.schemagen.graphql.GraphQLSchemaBuilder;
 import com.google.common.collect.ImmutableList;
-import com.google.gson.Gson;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import org.apache.commons.io.IOUtils;
 import org.molgenis.graphql.GsonTypeFactory;
 import org.molgenis.graphql.Metadata;
 import org.molgenis.ui.MolgenisPluginController;
@@ -14,14 +12,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static java.util.Objects.requireNonNull;
 import static org.molgenis.graphql.plugin.GraphQLPluginController.URI;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -33,21 +30,18 @@ public class GraphQLPluginController extends MolgenisPluginController
 {
 	public static final String ID = "graphql";
 	public static final String URI = MolgenisPluginController.PLUGIN_URI_PREFIX + ID;
-
 	private static final Logger LOG = LoggerFactory.getLogger(GraphQLPluginController.class);
 
 	private final GraphQL graphQL;
-	private final Gson gson;
 
 	@Autowired
-	public GraphQLPluginController(Metadata metadata, GsonTypeFactory gsonTypeFactory, Gson gson)
+	public GraphQLPluginController(Metadata metadata, GsonTypeFactory gsonTypeFactory)
 	{
 		super(URI);
-		this.gson = requireNonNull(gson);
 		GraphQLSchema schema = GraphQLSchemaBuilder.newBuilder().registerTypeFactory(gsonTypeFactory)
 				.registerGraphQLControllerObjects(ImmutableList.of(metadata)).build();
 		graphQL = new GraphQL(schema);
-		LOG.info("constructd");
+		LOG.info("constructe");
 	}
 
 	@RequestMapping(method = GET)
@@ -57,27 +51,20 @@ public class GraphQLPluginController extends MolgenisPluginController
 	}
 
 	@RequestMapping(method = POST, produces = APPLICATION_JSON_VALUE)
-	public
 	@ResponseBody
-	Object query(HttpServletRequest request) throws IOException
+	public Object executeOperation(@RequestBody Map body)
 	{
-		String queryJson = IOUtils.toString(request.getInputStream());
-		Map gsonMap = gson.fromJson(queryJson, Map.class);
-		String query = gsonMap.get("query").toString();
-		LOG.info("query: '{}'", query);
-
-		// now lets execute a query against the schema
-		ExecutionResult result = graphQL.execute(query);
-		if (result.getErrors().size() != 0)
+		String query = (String) body.get("query");
+		Map<String, Object> variables = (Map<String, Object>) body.get("variables");
+		ExecutionResult executionResult = graphQL.execute(query, (Object) null, variables);
+		Map<String, Object> result = new LinkedHashMap<>();
+		if (executionResult.getErrors().size() > 0)
 		{
-			LOG.error("Failed to execute query. Errors: {}", result.getErrors());
-			return result.getErrors();
+			result.put("errors", executionResult.getErrors());
+			LOG.error("Errors: {}", executionResult.getErrors());
 		}
-		else
-		{
-			LOG.info("Executed query. Data: {}", result.getData());
-			return result.getData();
-		}
+		result.put("data", executionResult.getData());
+		return result;
 	}
 
 }
